efeccdb0bf5b7be39fff4a0c7fd71d123443b0a6
G-omar-H <omarghazi12@gmail.com>
2024-04-13 21:56:46 +0000

    14. Iterate through object

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,12 +1,4 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let employeesStr = '';
-  for (const name of reportWithIterator) {
-    const temp = employeesStr;
-    if (temp.length) {
-      employeesStr = `${temp} | ${name}`;
-    } else {
-      employeesStr = name;
-    }
-  }
-  return employeesStr;
+  const employeesStr = [...reportWithIterator];
+  return employeesStr.join(' | ');
 }
