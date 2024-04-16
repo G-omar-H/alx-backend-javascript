2d3772b8a4d3ad90e3bc0ede23b2308ebeee193b
G-omar-H <omarghazi12@gmail.com>
2024-04-16 17:54:03 +0100

    0. Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,8 @@
 export default function getListStudents() {
-  const res = [];
-  const obj1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
-  res.push(obj1);
-  const obj2 = { id: 2, firstName: 'James', location: 'Columbia' };
-  res.push(obj2);
-  const obj3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
-  res.push(obj3);
-
-  return res;
+  const students = {
+    1: { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    2: { id: 2, firstName: 'James', location: 'Columbia' },
+    5: { id: 5, firstName: 'Serena', location: 'San Francisco' },
+  };
+  return Object.values(students);
 }
