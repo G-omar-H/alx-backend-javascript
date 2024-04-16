8be01d5cfbb14d05477cc76771845fabc3cc823c
G-omar-H <omarghazi12@gmail.com>
2024-04-16 22:40:15 +0100

    8. Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,12 +2,11 @@ export default function cleanSet(set, startString) {
   let result = '';
   set.forEach((value) => {
     if (value.startsWith(startString)) {
-      result += `${value.substring(startString.length)}-`;
+      if (result.length > 0) {
+        result += '-';
+      }
+      result += value.substring(startString.length);
     }
   });
-  // Remove the trailing hyphen if there are any values in the result
-  if (result.length > 0) {
-    result = result.slice(0, -1);
-  }
   return result;
 }
