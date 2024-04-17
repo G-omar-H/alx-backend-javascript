c5f3e0f8310e16798b6a76fd6b51cced31bf71d6
G-omar-H <omarghazi12@gmail.com>
2024-04-17 13:50:08 +0100

    1. Let`s build a Teacher interface

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,18 @@
+interface Teacher {
+  firstName: string;
+  lastName: string;
+  readonly fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; // Index signature for additional attributes
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false, // Example of additional attribute
+};
+
+console.log(teacher3);
