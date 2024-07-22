5d48a85821ffb526508600f72548c1d3c5e9de79
G-omar-H <omarghazi12@gmail.com>
2024-07-22 17:15:17 +0100

    2. Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function countStudents(filePath) {
+  try {
+    const data = fs.readFileSync(filePath, 'utf-8').split('\n');
+    if (!data || data.length === 0) {
+      throw new Error('Cannot load the database');
+    }
+
+    let count = 0;
+    const fields = {};
+    data.forEach((line, index) => {
+      if (index === 0 || !line) return;
+      const words = line.split(',');
+      if (words.length >= 4) {
+        count += 1;
+        const field = words[3];
+        if (!fields[field]) {
+          fields[field] = [];
+        }
+        fields[field].push(words[0]);
+      }
+    });
+    console.log(`Number of students: ${count}`);
+    for (const field in fields) {
+      if (Object.prototype.hasOwnProperty.call(fields, field)) {
+        console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+      }
+    }
+  } catch (error) {
+    console.error('Cannot load the database');
+  }
+}
+module.exports = countStudents;
