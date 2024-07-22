ff3841d3bd1904b8a0809fe2eb370cc5ad807b52
G-omar-H <omarghazi12@gmail.com>
2024-07-22 20:16:34 +0100

    2. Reading a file synchronously with Node JS

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -28,7 +28,7 @@ function countStudents(filePath) {
       }
     }
   } catch (error) {
-    console.error('Cannot load the database');
+    throw new Error('Cannot load the database');
   }
 }
 module.exports = countStudents;
