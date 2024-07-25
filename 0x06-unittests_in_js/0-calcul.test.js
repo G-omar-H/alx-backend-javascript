ca3b417b37f00cc5e3ce0149c5fa8812f339ec79
G-omar-H <omarghazi12@gmail.com>
2024-07-25 21:17:19 +0100

    0. Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,11 +1,24 @@
 var assert = require('assert');
+const {it, describe} = require('mocha');
 var calculateNumber = require('./0-calcul');
 
-describe('test round', function() {
-    it('check equality', function() {
+describe('calculateNumber', function() {
+    it('should return the sum of rounded numbers', function() {
         assert.equal(calculateNumber(1, 3), 4);
         assert.equal(calculateNumber(1, 3.7), 5);
         assert.equal(calculateNumber(1.2, 3.7), 5);
         assert.equal(calculateNumber(1.5, 3.7), 6);
+    });
+
+    it('should handle negative numbers', function() {
+        assert.equal(calculateNumber(-1.2, -3.7), -5);
+        assert.equal(calculateNumber(-1.5, -3.7), -5);
+        assert.equal(calculateNumber(-1.5, 3.7), 3);
+    });
+
+    it('should handle zero', function() {
+        assert.equal(calculateNumber(0, 0), 0);
+        assert.equal(calculateNumber(0.4, 0.4), 0);
+        assert.equal(calculateNumber(0.5, 0.5), 2);
     });
 });
