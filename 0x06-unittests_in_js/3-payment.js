47e6100416957eb06950cc267489fb3a3cd99173
G-omar-H <omarghazi12@gmail.com>
2024-07-28 19:37:32 +0100

    3. Spies

---
+++ 0x06-unittests_in_js/3-payment.js
@@ -0,0 +1,7 @@
+const Utils = require('./ utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+    return `The total is: ${total}`;
+}
+module.exports = sendPaymentRequestToApi
